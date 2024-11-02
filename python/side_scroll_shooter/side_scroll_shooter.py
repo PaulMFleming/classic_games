dbf6b62e8f378c7308701076cd1c4a92aca3d84a
Paul Fleming <paul.stillbreathing@gmail.com>
2024-11-02 11:54:26 +0000

    formatting

--- python/side_scroll_shooter/side_scroll_shooter.py
+++ python/side_scroll_shooter/side_scroll_shooter.py
@@ -2,29 +2,31 @@ import pygame
 import random
 
 from pygame.locals import (
-      RLEACCEL,
-      K_UP,
-      K_DOWN,
-      K_LEFT,
-      K_RIGHT,
-      K_ESCAPE,
-      K_SPACE,
-      KEYDOWN,
-      QUIT,
-      K_q,
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    K_SPACE,
+    KEYDOWN,
+    QUIT,
+    K_q,
 )
 
-pygame .init()
+pygame.init()
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75, 25))
+        self.surf = pygame.Surface((75, 25))  # width, height
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -32,25 +34,26 @@ player = Player()
 
 running = True
 
+# Main game loop
 while running:
-  for event in pygame.event.get():
-    if event.type == KEYDOWN:
-      if event.key == K_ESCAPE or event.key == K_q:
-        running = False
-    elif event.type == QUIT:
-      running = False
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE or event.key == K_q:
+                running = False
+        elif event.type == QUIT:
+            running = False
 
-  screen.fill((0, 0, 0))
+    screen.fill((0, 0, 0))
 
-  surf = pygame.Surface((50, 50))
-  surf.fill((255, 255, 255))
-  rect = surf.get_rect()
+    surf = pygame.Surface((50, 50))
+    surf.fill((255, 255, 255))
+    rect = surf.get_rect()
 
-  surf_center = (
-    (SCREEN_WIDTH-surf.get_width())/2,
-    (SCREEN_HEIGHT-surf.get_height())/2
-  )
+    surf_center = (
+        (SCREEN_WIDTH - surf.get_width()) / 2,
+        (SCREEN_HEIGHT - surf.get_height()) / 2,
+    )
 
-  screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
-  
-  pygame.display.flip()
+    screen.blit(player.surf, (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+
+    pygame.display.flip()
