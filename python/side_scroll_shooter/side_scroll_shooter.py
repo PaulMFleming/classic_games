04a702c1041974ca7c284c610385458e51deeb89
Paul Fleming <paul.stillbreathing@gmail.com>
2024-11-02 11:10:20 +0000

    add basic sprite (as surface)

--- python/side_scroll_shooter/side_scroll_shooter.py
+++ python/side_scroll_shooter/side_scroll_shooter.py
@@ -31,3 +31,16 @@ while running:
     elif event.type == QUIT:
       running = False
 
+  screen.fill((0, 0, 0))
+
+  surf = pygame.Surface((50, 50))
+  surf.fill((255, 255, 255))
+  rect = surf.get_rect()
+
+  surf_center = (
+    (SCREEN_WIDTH-surf.get_width())/2,
+    (SCREEN_HEIGHT-surf.get_height())/2
+  )
+
+  screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+  pygame.display.flip()
