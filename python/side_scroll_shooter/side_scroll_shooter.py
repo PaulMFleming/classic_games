aa6ade24030210597feb8cc40d7d17c4fe959cb2
Paul Fleming <paul.stillbreathing@gmail.com>
2024-11-02 11:13:47 +0000

    Player class

--- python/side_scroll_shooter/side_scroll_shooter.py
+++ python/side_scroll_shooter/side_scroll_shooter.py
@@ -19,7 +19,16 @@ pygame .init()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = Player()
 
 running = True
 
@@ -42,5 +51,6 @@ while running:
     (SCREEN_HEIGHT-surf.get_height())/2
   )
 
-  screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+  screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+  
   pygame.display.flip()
