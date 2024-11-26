9d98ab443169559a0af0c2a5a719a62d0808e5bb
Paul Fleming <paul.stillbreathing@gmail.com>
2024-11-26 17:33:00 +0000

    add background tile

--- python/vampire_survivor_clone/dungeon_survivor.py
+++ python/vampire_survivor_clone/dungeon_survivor.py
@@ -14,20 +14,35 @@ from pygame.locals import (
     K_q,
 )
 
-pygame.init()
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-running = True
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
+        self.bg_tile = pygame.image.load("images/background.png")
+        self.tile_size = 1000
 
-    screen.fill((0, 0, 0))
+        # self.player = Player(400, 300)
 
-    pygame.display.flip()
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+
+            self.screen.blit(self.bg_tile, (0, 0))
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+    pygame.quit()
