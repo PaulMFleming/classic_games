8cdfa540bfd771abcf584f60b170335325f88aa1
Paul Fleming <paul.stillbreathing@gmail.com>
2024-11-26 20:32:09 +0000

    finite map and camera scrolling

--- python/vampire_survivor_clone/dungeon_survivor.py
+++ python/vampire_survivor_clone/dungeon_survivor.py
@@ -17,6 +17,8 @@ from pygame.locals import (
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
+MAP_WIDTH = 4000
+MAP_HEIGHT = 3000
 
 
 class Player(pygame.sprite.Sprite):
@@ -24,7 +26,7 @@ class Player(pygame.sprite.Sprite):
         super(Player, self).__init__()
         self.surf = pygame.image.load("images/wizard_survivor_small.png")
         self.surf.set_colorkey((0, 0, 0), RLEACCEL)
-        self.rect = self.surf.get_rect()
+        self.rect = self.surf.get_rect(center=(x, y))
 
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
@@ -36,6 +38,35 @@ class Player(pygame.sprite.Sprite):
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(5, 0)
 
+        # Keep player on the screen
+        self.rect.clamp_ip(pygame.Rect(0, 0, MAP_WIDTH, MAP_HEIGHT))
+
+
+class Camera:
+    def __init__(self, width, height):
+        # camera is a rectangle that represents the portion of the game world that the player can see
+        self.camera = pygame.Rect(0, 0, width, height)
+        self.width = width
+        self.height = height
+
+    def apply(self, entity):
+        if isinstance(entity, pygame.Rect):
+            return entity.move(self.camera.topleft)
+        else:
+            return entity.rect.move(self.camera.topleft)
+
+    def update(self, target):
+        x = -target.rect.centerx + int(SCREEN_WIDTH / 2)
+        y = -target.rect.centery + int(SCREEN_HEIGHT / 2)
+
+        # limit scrolling to the world size
+        x = min(0, x)
+        y = min(0, y)
+        x = max(-(self.width - SCREEN_WIDTH), x)
+        y = max(-(self.height - SCREEN_HEIGHT), y)
+
+        self.camera = pygame.Rect(x, y, self.width, self.height)
+
 
 class Game:
     def __init__(self):
@@ -44,31 +75,42 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.bg_tile = pygame.image.load("images/background.png")
-        self.tile_size = 250
+        self.tile_size = self.bg_tile.get_width()
 
-        self.player = Player(400, 300)
+        self.player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        self.camera = Camera(MAP_WIDTH, MAP_HEIGHT)
 
     def run(self):
         running = True
         while running:
             for event in pygame.event.get():
-                if event.type == QUIT:
+                if event.type == QUIT or (
+                    event.type == KEYDOWN and event.key == K_ESCAPE
+                ):
                     running = False
-
-            for x in range(0, SCREEN_WIDTH, self.tile_size):
-                for y in range(0, SCREEN_HEIGHT, self.tile_size):
-                    self.screen.blit(self.bg_tile, (x, y))
 
             pressed_keys = pygame.key.get_pressed()
             self.player.update(pressed_keys)
+            self.camera.update(self.player)
 
-            self.screen.blit(self.bg_tile, (0, 0))
-            self.screen.blit(self.player.surf, self.player.rect)
+            # Tile the background
+            for x in range(0, MAP_WIDTH, self.tile_size):
+                for y in range(0, MAP_HEIGHT, self.tile_size):
+                    self.screen.blit(
+                        self.bg_tile,
+                        self.camera.apply(
+                            pygame.Rect(x, y, self.tile_size, self.tile_size)
+                        ),
+                    )
+
+            self.screen.blit(self.player.surf, self.camera.apply(self.player))
+
             pygame.display.flip()
             self.clock.tick(60)
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
     game = Game()
     game.run()
-    pygame.quit()
